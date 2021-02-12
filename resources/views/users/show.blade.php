ba6df485f9f9ffb22082dedccfcbb8fcaff67cbd
Morgan Gonzales <morgan.gonzales@cloudpay.net>
2021-02-12 22:01:44 +0800

    Improve show user display

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,20 +3,26 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card mb-3" style="max-width: 540px;">
+        <div class="col-md-12">
+            <div class="card mb-3">
                 <div class="row no-gutters">
                     <div class="col-md-4">
                         <img src="{{ asset($user->photo) }}" class="card-img" alt="...">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <h5 class="card-title">{{ $user->username }} </h5>
-                            <div class="cart-text">
+                            <h1 class="card-title">{{ $user->username }} </h1>
+                            <dl class="row">
                                 @foreach($user->details as $detail)
-                                    <p>{{ $detail->key }}: {{ $detail->value }}</p>
+                                <dt class="col-sm-3">{{ $detail->key }}</dt>
+                                <dd class="col-sm-9">{{ $detail->value ?: 'N/A' }}</dd>
                                 @endforeach
-                            </div>
+                            </dl>
+{{--                            <div class="cart-text">--}}
+{{--                                @foreach($user->details as $detail)--}}
+{{--                                    <p>{{ $detail->key }}: {{ $detail->value ?: 'N/A' }}</p>--}}
+{{--                                @endforeach--}}
+{{--                            </div>--}}
                         </div>
                     </div>
                 </div>
